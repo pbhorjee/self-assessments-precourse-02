89d6698453079f6a33ac4d6914d3e1d8de716e0c
Alex Zywiak <alex.zywiak@gmail.com>
2015-09-14 13:30:37 -0400

    Implemented _.memoize()

--- underbar-memoize/underbar-memoize.js
+++ underbar-memoize/underbar-memoize.js
@@ -2,15 +2,30 @@ var _ = {};
 
 (function() {
 
-  // Memorize an expensive function's results by storing them. You may assume
-  // that the function takes only one argument and that it is a primitive.
-  // memoize could be renamed to oncePerUniqueArgumentList; memoize does the
-  // same thing as once, but based on many sets of unique arguments.
-  //
-  // _.memoize should return a function that, when called, will check if it has
-  // already computed the result for the given argument and return that value
-  // instead if possible.
-  _.memoize = function(func) {
-  };
+    // Memorize an expensive function's results by storing them. You may assume
+    // that the function takes only one argument and that it is a primitive.
+    // memoize could be renamed to oncePerUniqueArgumentList; memoize does the
+    // same thing as once, but based on many sets of unique arguments.
+    //
+    // _.memoize should return a function that, when called, will check if it has
+    // already computed the result for the given argument and return that value
+    // instead if possible.
+    _.memoize = function(func) {
+
+        var memos = {},
+            res,
+            arg;
+
+        return function() {
+            arg = arguments[0];
+            if (!memos[arg]) {
+                res = func(arg);
+                memos[arg] = res;
+            } else {
+                res = memos[arg];
+            }
+            return res;
+        };
+    };
 
 }).call(this);
