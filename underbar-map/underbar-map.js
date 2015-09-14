8b67c83c467ff256792e6e255956a069b4c97e48
lesliepajuelo <l.l.pajuelo@gmail.com>
2015-09-14 18:24:19 -0500

    Complete _.map

--- underbar-map/underbar-map.js
+++ underbar-map/underbar-map.js
@@ -8,6 +8,12 @@ var _ = {};
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+
+    var result = [];
+
+    collection.forEach(function(item, index, collection){
+      result.push(iterator(item, index, collection))
+    })
   };
 
 }).call(this);
