adf04be498ff4eecbc746e985a50eb62866e0add
lesliepajuelo <l.l.pajuelo@gmail.com>
2015-09-14 18:38:26 -0500

    Complete _.uniq

--- underbar-uniq/underbar-uniq.js
+++ underbar-uniq/underbar-uniq.js
@@ -5,6 +5,15 @@ var _ = {};
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+        
+   var result = [];
+
+     array.forEach(function(index){
+      if(array.indexOf(result, index) === -1){
+        result.push(index);
+      }
+     });
+    return arr;
   };
 
 }).call(this);
