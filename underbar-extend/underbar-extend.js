bcf4fc5b185d3707772714a5afc5065ab4895d42
Alex Zywiak <alex.zywiak@gmail.com>
2015-09-14 13:05:27 -0400

    Implemented _.extend()

--- underbar-extend/underbar-extend.js
+++ underbar-extend/underbar-extend.js
@@ -14,7 +14,20 @@ var _ = {};
   //   }, {
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
+  
   _.extend = function(obj) {
+    var args = arguments,
+        i,
+        prop;
+
+    for(i = 1; i < args.length; i++){
+      for(prop in args[i]){
+        if(!obj[prop]){
+          obj[prop] = args[i][prop];
+        }
+      }
+    }
+    return obj;
   };
 
 }).call(this);
