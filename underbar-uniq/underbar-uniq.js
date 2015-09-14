1a102df491fc8648c9bc8a1129d8180aa92bd710
Michael Kim <kim.michael88@gmail.com>
2015-09-14 13:34:45 -0700

    Implement _.uniq function

--- underbar-uniq/underbar-uniq.js
+++ underbar-uniq/underbar-uniq.js
@@ -2,9 +2,24 @@ var _ = {};
 
 (function() {
 
+  _.contains = function(array, value) {
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] === value) {
+        return true;
+      }
+    }
+    return false;
+  }
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var output = [];
+    for (var i = 0; i < array.length; i++) {
+      if (!_.contains(output, array[i])) {
+        output.push(array[i]);
+      }
+    }
+    return output;
   };
 
 }).call(this);
