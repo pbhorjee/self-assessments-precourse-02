7020e23b4449c63348dea9d86d7895b2f0442760
Alex Zywiak <alex.zywiak@gmail.com>
2015-09-14 13:46:10 -0400

    Implemented _.uniq()

--- underbar-uniq/underbar-uniq.js
+++ underbar-uniq/underbar-uniq.js
@@ -3,8 +3,33 @@ var _ = {};
 (function() {
 
 
-  // Produce a duplicate-free version of the array.
-  _.uniq = function(array) {
-  };
+    // Produce a duplicate-free version of the array.
+    _.uniq = function(array) {
+
+        var item,
+            res = [],
+            i;
+
+        var contains = function(collection, target) {
+            var i,
+                item;
+
+            for (i = 0; i < collection.length; i++) {
+                item = collection[i];
+                if (item === target) {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        for (i = 0; i < array.length; i++) {
+            item = array[i];
+            if(!contains(res, item)){
+              res.push(item);
+            }
+        }
+        return res;
+    };
 
 }).call(this);
