8cebc902608458ba2c29e338b98ad9ac3be4fad7
Brian Kustra <bkustrakb@gmail.com>
2015-09-14 21:14:42 -0500

    Implements _.extends

--- underbar-extend/underbar-extend.js
+++ underbar-extend/underbar-extend.js
@@ -15,6 +15,13 @@ var _ = {};
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+     var args = Array.prototype.slice.call(arguments, 1);
+     for (var i = 0; i < args.length; i++) {
+       for (var key in args[i]) {
+        obj[key] = args[i][key];
+       }
+     }
+     return obj;
   };
 
 }).call(this);
