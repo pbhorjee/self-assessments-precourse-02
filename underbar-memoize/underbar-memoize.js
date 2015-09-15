042298b02ac33eacc367abd29ed5b44295b3dfc9
Brian Kustra <bkustrakb@gmail.com>
2015-09-14 21:01:55 -0500

    Implemented _.memoize

--- underbar-memoize/underbar-memoize.js
+++ underbar-memoize/underbar-memoize.js
@@ -11,6 +11,15 @@ var _ = {};
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+    var memo = {};
+    var args = Array.prototype.slice.call(arguments)
+    return function() {
+      if (args in memo) {
+        return memo[args];
+      } else {
+        return memo[args] = func.apply(this, args);
+      }
+    }
   };
 
 }).call(this);
