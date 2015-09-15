c0a1a4e6a9d0410e2a32efbb3c59dd9baf6caca4
Ryan Price <rsprice90@gmail.com>
2015-09-15 00:42:18 -0700

    Implement _.extend. 	modified:   underbar-extend/underbar-extend.js

--- underbar-extend/underbar-extend.js
+++ underbar-extend/underbar-extend.js
@@ -14,7 +14,27 @@ var _ = {};
   //   }, {
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
+
   _.extend = function(obj) {
+    // Hold on to the first object so we can extend it.
+    var extendedObj = arguments[0];
+    
+    // For the properties in the arguments object,
+    // which we know will be objects.
+    for (var prop in arguments) {
+      // Hold on to the current object being iterated over.
+      var currentObj = arguments[prop];
+      // Iterate through the current object.
+      for (var key in currentObj) {
+        // If the object we want to extend doesn't have a value for the key in the current object.
+        if (extendedObj[key] === undefined) {
+          // Assign the key/value pair for the current key to the object we want to extend.
+          extendedObj[key] = currentObj[key];
+        }
+      }
+    }
+    // finally, return the extended object.
+    return extendedObj;
   };
 
 }).call(this);
