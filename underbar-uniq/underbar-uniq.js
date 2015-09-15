f5a1d1dcbe2f27383b365ebd7325ee7154caa614
amenotu <kyabut@gmail.com>
2015-09-15 06:28:10 -0700

    Implemented _.uniq

--- underbar-uniq/underbar-uniq.js
+++ underbar-uniq/underbar-uniq.js
@@ -5,6 +5,14 @@ var _ = {};
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var sorted = array.sort();
+    var results = [];
+    for(var i = 0; i < array.length; i++){
+      if(array[0+i] !== array[1+i]){
+        results.push(array[i])
+      }
+    }
+    return results;
   };
 
 }).call(this);
