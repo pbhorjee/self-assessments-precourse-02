60042f842f31b173ebb2f68c6d55d4eb119d8f40
Anthony Mullins <anthonyjoelmullins@gmail.com>
2015-09-14 19:29:11 -0600

    Solve assessment _.extend

--- underbar-extend/underbar-extend.js
+++ underbar-extend/underbar-extend.js
@@ -15,6 +15,15 @@ var _ = {};
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    // loop through all arguments that are passed in
+    for(var i = 0; i < arguments.length; i++) {
+      // loop through the object stored in each respective arguement index
+      for(var key in arguments[i]) {
+        // Update properties in the original obj w/properties from the passed in object(s)
+        obj[key] = arguments[i][key];
+      }
+    }
+    return obj;
   };
 
 }).call(this);
