134957aff9068f50cd6b5194fde1f20c2dd51641
eakray <ekrayni@gmail.com>
2015-09-14 22:23:34 -0500

    implemented _.uniq

--- underbar-uniq/underbar-uniq.js
+++ underbar-uniq/underbar-uniq.js
@@ -5,6 +5,17 @@ var _ = {};
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+  var i;
+  var unique = [];
+  var logged = [array[0]];
+
+  for (i = 0; i < array.length; i++){
+    if (_.contains(unique, array[i])=== false){
+      unique.push(array[i]);
+    }
+      logged.push(array[i]);
+    }
+    return unique;
   };
 
 }).call(this);
