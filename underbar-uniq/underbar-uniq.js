47e1df5b52545e5de0304dbca4b44fe422a18b4d
Ryan Price <rsprice90@gmail.com>
2015-09-15 00:53:00 -0700

    Implement _.uniq. 	modified:   underbar-uniq/underbar-uniq.js

--- underbar-uniq/underbar-uniq.js
+++ underbar-uniq/underbar-uniq.js
@@ -5,6 +5,30 @@ var _ = {};
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    // Create an array for results.
+    var results = [];
+    // Variable that will hold temporary values
+    var currentEle;
+
+    // For each element in the array
+    array.forEach(function(element) {
+      // currentEle is set to the current element
+      currentEle = element;
+      // if the current element does not yet exist in results
+      if (results.indexOf(currentEle) === -1) {
+        // push the current element to results
+        results.push(currentEle);
+      }
+    });
+    // return the array of unique values
+    return results;
   };
 
 }).call(this);
+
+// remove the duplicates from an array like this one.
+// [1, 2, 3, 4, 5, 1, 2, 3]...
+
+var test1 = _.uniq([1, 2, 3, 4, 5, 1, 2, 3]);
+
+console.log(test1)
