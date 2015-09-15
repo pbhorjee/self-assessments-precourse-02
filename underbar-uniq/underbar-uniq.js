05b6bdc325fc6fb2f2c778b4776f69b5e73679d1
Omar Sial <omar.sial1@gmail.com>
2015-09-15 00:38:16 -0400

    Functions complete

--- underbar-uniq/underbar-uniq.js
+++ underbar-uniq/underbar-uniq.js
@@ -5,6 +5,13 @@ var _ = {};
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var result = [];
+    array.forEach(function(a) {
+      if (result.indexOf(a) === -1) {
+        result.push(a);
+      }
+    });
+    return result;
   };
 
 }).call(this);
