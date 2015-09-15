46d4689094c49ec098e127bddb38497f22fb395e
Seth Roberts <seth.b.roberts (at) gmail (dot) com>
2015-09-15 11:46:40 -0400

    underbar 2 assessment complete

--- underbar-uniq/underbar-uniq.js
+++ underbar-uniq/underbar-uniq.js
@@ -1,10 +1,28 @@
 var _ = {};
 
+var indexOf = function (arr, target) {
+  var result = -1;
+  for (var i=0; i<arr.length; i++) {
+    if (arr[i] === target) {
+      result = i;
+    }
+  }
+  return result;
+};
+
 (function() {
 
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var result = [];
+    for (var i=0; i<array.length; i++) {
+      if (indexOf(result, array[i]) < 0) {
+        result.push(array[i]);
+      }
+    }
+
+    return result;
   };
 
 }).call(this);
