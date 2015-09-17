e7308d494788a29d38341bdcd2681dadb7ac2ceb
mgaff <mgaffney123@gmail.com>
2015-09-17 10:33:37 -0400

    Finished Self Assessment #2

--- underbar-uniq/underbar-uniq.js
+++ underbar-uniq/underbar-uniq.js
@@ -5,6 +5,14 @@ var _ = {};
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var finalArray = [];
+    _.each(array, function(item) {
+        if ((_.indexOf(finalArray, item)) === -1) {
+          finalArray.push(item);
+        }
+    });
+    return finalArray;
+
   };
 
 }).call(this);
