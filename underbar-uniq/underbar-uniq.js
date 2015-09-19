e0e2b69c29ae0f689077f52bff858668b4a32ab9
JieYing Peng <jipeng@syr.edu>
2015-09-19 14:25:54 -0400

    solved all prompts

--- underbar-uniq/underbar-uniq.js
+++ underbar-uniq/underbar-uniq.js
@@ -5,6 +5,15 @@ var _ = {};
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
-  };
+		var obj = {};
+        var result = [];
+        array.forEach(function(item) {
+            obj[item] = item;
+        });
+        obj.forEach(function(value) {
+            result.push(value);
+        });
+        return result;
+    };
 
 }).call(this);
