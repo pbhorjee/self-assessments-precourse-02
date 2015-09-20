045f837de9868a0947046f3903aeededb308808d
NickPapayiannakis <nikos.papayiannakis@gmail.com>
2015-09-20 08:15:27 -0400

    Removed mixed tabs and spaces

--- underbar-map/underbar-map.js
+++ underbar-map/underbar-map.js
@@ -1,8 +1,6 @@
 var _ = {};
 
 (function() {
-
-
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
     // map() is a useful primitive iteration function that works a lot
@@ -13,15 +11,15 @@ var _ = {};
     var resultObject = {};
 
     if (Array.isArray(collection)){
-    	for (var i = 0; i < collection.length; i++) {
-    		resultArray.push(iterator(collection[i]));
-    	}
-    	return resultArray;
+      for (var i = 0; i < collection.length; i++) {
+        resultArray.push(iterator(collection[i]));
+      }
+      return resultArray;
     } else if (typeof collection === 'object') { 
-    	for (var key in collection) {
-    		resultObject[key] = iterator(collection[key]);
-    	}
-    	return resultObject;
-    } 
+      for (var key in collection) {
+        resultObject[key] = iterator(collection[key]);
+      }
+      return resultObject;
+    }
   };
 }).call(this);
