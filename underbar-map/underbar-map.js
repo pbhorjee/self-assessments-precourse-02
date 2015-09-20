abc5f629327c52b9cf64021670b95b5445e484ee
NickPapayiannakis <nikos.papayiannakis@gmail.com>
2015-09-20 08:09:33 -0400

    Made useable for both objects and arrays.  Refactored code and removed comments.

--- underbar-map/underbar-map.js
+++ underbar-map/underbar-map.js
@@ -9,24 +9,19 @@ var _ = {};
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
 
-    //declare a variable to hold the new array or object.
-    var result;
-    //determine whether collection is an object or an array
-    //if object, traverse array
+    var resultArray = [];
+    var resultObject = {};
+
     if (Array.isArray(collection)){
     	for (var i = 0; i < collection.length; i++) {
-    		//for each element, apply the iterator and push it to result array
-    		result.push(iterator(collection[i]));
+    		resultArray.push(iterator(collection[i]));
     	}
-    } else if (typeof collection === 'object') {  //else if collection is an object
-    	//for each key, apply the iterator to the value
+    	return resultArray;
+    } else if (typeof collection === 'object') { 
     	for (var key in collection) {
-    		//place new/modified property in result object
-    		result[key] = iterator(collection[key]);
+    		resultObject[key] = iterator(collection[key]);
     	}
-    }
-
-    return result;
+    	return resultObject;
+    } 
   };
-
 }).call(this);
