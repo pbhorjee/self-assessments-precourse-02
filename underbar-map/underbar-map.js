7e746e31855f0254b93d39e660d45bb35f85f78e
rascle <ryan.bascle@gmail.com>
2015-09-20 17:32:07 -0500

    Completed the second assessment.

--- underbar-map/underbar-map.js
+++ underbar-map/underbar-map.js
@@ -5,6 +5,11 @@ var _ = {};
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
+    var mapped = [];
+    _.each(collection, function(val) {
+      mapped.push(iterator(val));
+    });
+    return mapped;
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
