732e838b57a0959d9fda0bc33c2eb7b2ea7509ed
nissaw <nissa.wollum@gmail.com>
2015-09-20 10:49:27 -0700

    Uniq completed

--- underbar-uniq/underbar-uniq.js
+++ underbar-uniq/underbar-uniq.js
@@ -4,7 +4,14 @@ var _ = {};
 
 
   // Produce a duplicate-free version of the array.
-  _.uniq = function(array) {
-  };
+	_.uniq = function(array) {
+		var result = [];
+		for (var i = 0; i < array.length; i++) {
+			if (result.indexOf(array[i]) < 0) {
+				result.push(array[i])
+			}
+		}
+		return result;
+	};
 
 }).call(this);
