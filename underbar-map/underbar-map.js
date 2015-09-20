e905a0837acd3104016629fbefddc852e6257d1e
nissaw <nissa.wollum@gmail.com>
2015-09-20 10:10:46 -0700

    Map Completed

--- underbar-map/underbar-map.js
+++ underbar-map/underbar-map.js
@@ -5,9 +5,14 @@ var _ = {};
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
-    // map() is a useful primitive iteration function that works a lot
-    // like each(), but in addition to running the operation on all
-    // the members, it also maintains an array of results.
+    var result = [];
+    _.each(collection, function(element) {
+      result.push(iterator(element));
+    })
+    return result;
+// map() is a useful primitive iteration function that works a lot
+// like each(), but in addition to running the operation on all
+// the members, it also maintains an array of results.
   };
 
 }).call(this);
