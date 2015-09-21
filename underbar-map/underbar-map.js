8adfe53c02695f73cd88345fe583a9d61608e0f7
Alice <green.alice.e@gmail.com>
2015-09-21 20:44:22 +0000

    "Updated Map"

--- underbar-map/underbar-map.js
+++ underbar-map/underbar-map.js
@@ -2,7 +2,17 @@ var _ = {};
 
 (function() {
 
-
+  _.each = function(collection, iterator) {
+    if(Array.isArray(collection)) {
+      for(var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for(var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
+  };
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
     // map() is a useful primitive iteration function that works a lot
