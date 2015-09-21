3dfe6a21ca008c645f833db388015863bbf88e1f
Alice <green.alice.e@gmail.com>
2015-09-21 20:29:45 +0000

    "Self Assessment Underbar Map"

--- underbar-map/underbar-map.js
+++ underbar-map/underbar-map.js
@@ -8,6 +8,11 @@ var _ = {};
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var output = [];
+    _.each(collection, function(value) {
+      output.push(iterator(value));
+    });
+    return output;
   };
 
 }).call(this);
