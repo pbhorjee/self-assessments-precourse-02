be12c7b5b6e4a46ae6e4b9bf976945b040dbdff9
Alice <green.alice.e@gmail.com>
2015-09-21 20:34:03 +0000

    "Self Assessment Uniq Complete"

--- underbar-uniq/underbar-uniq.js
+++ underbar-uniq/underbar-uniq.js
@@ -5,6 +5,13 @@ var _ = {};
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var output = [];
+    _.each(array, function(value) {
+      if(_.indexOf(output, value) === -1) {
+        output.push(value);
+      }
+    });
+    return output;
   };
 
 }).call(this);
