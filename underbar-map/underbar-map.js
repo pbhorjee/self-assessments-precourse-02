f34a1e9182c1b58d8f2142e237d100c656c86f64
RobertBowie <robertb1903@gmail.com>
2015-09-20 23:50:51 -0700

    Completed _.map()

--- underbar-map/underbar-map.js
+++ underbar-map/underbar-map.js
@@ -2,12 +2,29 @@ var _ = {};
 
 (function() {
 
+  // Quick _.each() rewrite to help map stay clean:
+  _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else if (typeof(collection) === 'object') {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
+  };
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var result = [];
+    _.each(collection, function(value) {
+      result.push(iterator(value));
+    });
+    return result;
   };
 
 }).call(this);
