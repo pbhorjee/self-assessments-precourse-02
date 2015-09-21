f9ad62dd14968505e17dba5ed57f471e50893152
Alice <green.alice.e@gmail.com>
2015-09-21 20:39:28 +0000

    "Added each and indexOf"

--- underbar-uniq/underbar-uniq.js
+++ underbar-uniq/underbar-uniq.js
@@ -1,8 +1,28 @@
 var _ = {};
 
 (function() {
+   _.each = function(collection, iterator) {
+    if(Array.isArray(collection)) {
+      for(var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for(var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
+  };
+  
+  _.indexOf = function(array, target){
+    var result = -1;
+    _.each(array, function(item, index) {
+      if (item === target && result === -1) {
+        result = index;
+      }
+    });
 
-
+    return result;
+  };
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
     var output = [];
