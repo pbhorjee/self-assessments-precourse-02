202883c05927bf433ae64a271a1be016bb72bbde
NickPapayiannakis <nikos.papayiannakis@gmail.com>
2015-09-20 22:42:39 -0400

    Attempted completion of _.memoize

--- underbar-memoize/underbar-memoize.js
+++ underbar-memoize/underbar-memoize.js
@@ -10,7 +10,21 @@ var _ = {};
   // _.memoize should return a function that, when called, will check if it has
   // already computed the result for the given argument and return that value
   // instead if possible.
-  _.memoize = function(func) {
-  };
 
+  _.memoize = function(func) {
+    //store outputs with their given inputs
+    var cache = {};
+    var output = func;
+
+    return function(input) {
+      //check if the input has been run before, if not
+      if (!(input in cache)) {
+        //run the function, store the output with the input, and return it
+        cache[input] = output; //Don't know how to run original function here...
+        return cache[input];
+      } else {
+        return cache[input];
+      }
+    }
+  };
 }).call(this);
