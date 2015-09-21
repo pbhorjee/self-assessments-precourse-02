c996ed9e6f8b33f71e1b050256a55d3129a7c186
RobertBowie <robertb1903@gmail.com>
2015-09-21 00:00:19 -0700

    Completed _.uniq()

--- underbar-uniq/underbar-uniq.js
+++ underbar-uniq/underbar-uniq.js
@@ -2,9 +2,27 @@ var _ = {};
 
 (function() {
 
-
+  // _.each() rewrite to help _.uniq() stay clean:
+  _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else if (typeof(collection) === 'object') {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
+  };
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var result = [];
+    _.each(array.sort(), function(val, ind, coll) {
+      if(coll[ind + 1] !== val) {
+        result.push(val);
+      }
+    });
+    return result;
   };
 
 }).call(this);
