a6d06e794f39e51685dd705642e895328c306b27
Victor Carter <Victor.A.York@gmail.com>
2015-09-22 17:51:21 -0500

    fixed implementation: added other helper functions

--- underbar-uniq/underbar-uniq.js
+++ underbar-uniq/underbar-uniq.js
@@ -2,10 +2,22 @@ var _ = {};
 
 (function () {
 
-    _.contains = function(collection, queryItem){
+    _.each = function (collection, action) {
+        if (Array.isArray(collection)) {
+            for (var i = 0; i < collection.length; i++) {
+                action(collection[i], i, collection);
+            }
+        } else {
+            for (var key in collection) {
+                action(collection[key], key, collection);
+            }
+        }
+    };
+
+    _.contains = function (collection, queryItem) {
         var result = false;
-        _.each(collection, function(item){
-            if (item === queryItem){
+        _.each(collection, function (item) {
+            if (item === queryItem) {
                 result = true;
             }
         });
@@ -14,8 +26,8 @@ var _ = {};
     // Produce a duplicate-free version of the array.
     _.uniq = function (array) {
         var uniqueElemets = [];
-        _.each(array, function(item){
-            if (!_.contains(uniqueElemets, item)){
+        _.each(array, function (item) {
+            if (!_.contains(uniqueElemets, item)) {
                 uniqueElemets.push(item);
             }
         });
